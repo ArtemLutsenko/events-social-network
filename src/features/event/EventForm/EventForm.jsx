83a7e2bf8be2f9aedb9879a6d6caeeaaae9aaf8a
ArtemLutsenko <artem.lutsenko@gmail.com>
2019-09-02 07:54:19 +0300

    add form

---
+++ src/features/event/EventForm/EventForm.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import { Segment, Form, Button } from "semantic-ui-react";
+
+export default class EventForm extends Component {
+  state = {
+    title: "",
+    date: "",
+    city: "",
+    venue: "",
+    hostedBy: ""
+  };
+  handleFormSubmit = evt => {
+    evt.preventDefault();
+    this.props.createEvent(this.state)
+  };
+
+  handleInputChange = ({target:{name, value}}) => {
+    this.setState({
+      [name]: value
+    });
+  };
+  render() {
+    const { title, date, city, venue, hostedBy } = this.state;
+    return (
+      <Segment>
+        <Form onSubmit={this.handleFormSubmit} autoComplete="off">
+          <Form.Field>
+            <label>Event Title</label>
+            <input
+              placeholder="Event Title"
+              value={title}
+              onChange={this.handleInputChange}
+              name="title"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Event Date</label>
+            <input
+              type="date"
+              value={date}
+              onChange={this.handleInputChange}
+              name="date"
+              placeholder="Event Date"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>City</label>
+            <input
+              placeholder="City event is taking place"
+              value={city}
+              onChange={this.handleInputChange}
+              name="city"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Venue</label>
+            <input
+              placeholder="Enter the Venue of the event"
+              value={venue}
+              onChange={this.handleInputChange}
+              name="venue"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Hosted By</label>
+            <input
+              placeholder="Enter the name of person hosting"
+              value={hostedBy}
+              onChange={this.handleInputChange}
+              name="hostedBy"
+            />
+          </Form.Field>
+          <Button positive type="submit">
+            Submit
+          </Button>
+          <Button type="button" onClick={this.props.cancelFormOpen}>
+            Cancel
+          </Button>
+        </Form>
+      </Segment>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
-const rootEl = document.getElementById('root')
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
 
 
 serviceWorker.unregister();
